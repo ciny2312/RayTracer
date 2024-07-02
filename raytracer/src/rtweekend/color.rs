6ca55cc2d7b5549fef209b50abc11adf6e988cf6
ciny2312 <ciny2312@sjtu.edu.cn>
2024-07-02 16:29:01 +0800

    image12

--- raytracer/src/rtweekend/color.rs
+++ raytracer/src/rtweekend/color.rs
@@ -3,10 +3,21 @@ use crate::rtweekend::vec3::Color;
 use std::fs::File;
 use std::io::Write;
 
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        return linear_component.sqrt();
+    }
+    0.0
+}
 pub fn write_color(pixel_color: &Color, file: &mut File) {
-    let r = pixel_color.e[0];
-    let g = pixel_color.e[1];
-    let b = pixel_color.e[2];
+    let mut r = pixel_color.e[0];
+    let mut g = pixel_color.e[1];
+    let mut b = pixel_color.e[2];
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
+
     let intensity = Interval {
         min: 0.000,
         max: 0.999,
