4b1817419844ec9dbf36c2fae9d983f11732d831
ciny2312 <ciny2312@sjtu.edu.cn>
2024-07-08 05:55:40 +0800

    b2_img10

--- raytracer/src/hittable_list/perlin.rs
+++ raytracer/src/hittable_list/perlin.rs
@@ -25,11 +25,40 @@ impl Perlin {
         }
     }
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = ((4_f64 * p.e[0]) as i32) & 255;
-        let j = ((4_f64 * p.e[1]) as i32) & 255;
-        let k = ((4_f64 * p.e[2]) as i32) & 255;
+        let u = p.e[0] - p.e[0].floor();
+        let v = p.e[1] - p.e[1].floor();
+        let w = p.e[2] - p.e[2].floor();
 
-        self.randfloat[(self.x[i as usize] ^ self.y[j as usize] ^ self.z[k as usize]) as usize]
+        let i = p.e[0].floor() as i32;
+        let j = p.e[1].floor() as i32;
+        let k = p.e[2].floor() as i32;
+        let mut c: [[[f64; 3]; 3]; 3] = [[[0.0, 0.0, 0.0]; 3]; 3];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.randfloat[(self.x[(i as usize + di) & 255]
+                        ^ self.y[(j as usize + dj) & 255]
+                        ^ self.z[(k as usize + dk) & 255])
+                        as usize];
+                }
+            }
+        }
+        Self::trilinear_interp(&c, u, v, w)
+    }
+    fn trilinear_interp(c: &[[[f64; 3]; 3]; 3], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1 - i) as f64 * (1.0 - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
     fn permute(p: &mut [i32; POINT_COUNT], n: usize) {
         for i in 0..n {
